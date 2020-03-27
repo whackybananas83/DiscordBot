a3ed2aeac4231719c46072a19d009aca61627faf
Ronald <Rlemuel1992@gmail.com>
2020-03-26 23:27:50 -0400

    asd

---
+++ BotManager.cs
@@ -0,0 +1,32 @@
+﻿using Discord;
+using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordBot
+{
+    class BotManager
+    {
+        private DiscordSocketClient discordClient;
+        private DiscordToken token;
+
+        public BotManager()
+        {
+            token = new DiscordToken();
+            discordClient = new DiscordSocketClient();
+        }
+
+        public async Task InitializeBot()
+        {
+            discordClient.Log += Log;
+
+        }
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,26 +6,8 @@ using System.Threading.Tasks;
 namespace DiscordBot
 {
     class Program
-    {
-
-        private DiscordSocketClient discordClient;
-
+    { 
         static void Main(string[] args) =>
-            new Program().MainAsync().GetAwaiter().GetResult();
-
-        public async Task MainAsync()
-        {
-            discordClient = new DiscordSocketClient();
-
-            discordClient.Log += Log;
-
-            var token = "";
-        }
-
-        private Task Log(LogMessage msg)
-        {
-            Console.WriteLine(msg.ToString());
-            return Task.CompletedTask;
-        }
+            new BotManager().InitializeBot().GetAwaiter().GetResult();
     }
 }
