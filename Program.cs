95a654073de4fd6350f53d8fd2f36859ad80c4ce
Ronald <Rlemuel1992@gmail.com>
2020-03-26 23:05:38 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using Discord;
+using Discord.WebSocket;
+using System;
+using System.Threading.Tasks;
+
+namespace DiscordBot
+{
+    class Program
+    {
+
+        private DiscordSocketClient discordClient;
+
+        static void Main(string[] args) =>
+            new Program().MainAsync().GetAwaiter().GetResult();
+
+        public async Task MainAsync()
+        {
+            discordClient = new DiscordSocketClient();
+
+            discordClient.Log += Log;
+
+            var token = "";
+        }
+
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+    }
+}
